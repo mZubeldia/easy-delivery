74b5c64a82da3108046c11390550fb1db6cd0e61
mZubeldia <mzubeldia09@gmail.com>
2021-08-28 13:53:12 +0200

    add bootstrap template

--- easy-delivery-bootstrap/src/App.js
+++ easy-delivery-bootstrap/src/App.js
@@ -1,38 +1,149 @@
-import "./App.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import { Button, Alert, Breadcrumb, Card, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form className="mb-3">
-          <Form.Group>
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="example@email.com" />
-            <Form.Text className="text-muted">
-              We'll never share your email address!
-            </Form.Text>
-          </Form.Group>
-        </Form>
-
-        <Card className="mb-3" style={{ color: "#000" }}>
-          <Card.Img src="https://picsum.photos/200/50" />
-          <Card.Body>
-            <Card.Title>Card Example</Card.Title>
-            <Card.Text>This is a bootstrap react card</Card.Text>
-            <Button variant="danger">Read more</Button>
-          </Card.Body>
-        </Card>
-        <Breadcrumb>
-          <Breadcrumb.Item>Test</Breadcrumb.Item>
-          <Breadcrumb.Item>Test 2</Breadcrumb.Item>
-          <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
-        </Breadcrumb>
-        <Alert variant="secondary">Holi</Alert>
-        <Button> Test button</Button>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container px-5">
+            <a class="navbar-brand" href="#!">
+              Start Bootstrap
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#!">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#!">
+                    About
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#!">
+                    Contact
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#!">
+                    Services
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
+
+      <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+              alt="..."
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Business Name or Tagline</h1>
+            <p>
+              This is a template that is great for small businesses. It doesn't
+              have too much fancy flare to it, but it makes a great use of the
+              standard Bootstrap core components. Feel free to use this template
+              for any project you want!
+            </p>
+            <a class="btn btn-primary" href="#!">
+              Call to Action!
+            </a>
+          </div>
+        </div>
+
+        <div class="card text-white bg-secondary my-5 py-4 text-center">
+          <div class="card-body">
+            <p class="text-white m-0">
+              This call to action card is a great place to showcase some
+              important information or display a clever tagline!
+            </p>
+          </div>
+        </div>
+
+        <div class="row gx-4 gx-lg-5">
+          <div class="col-md-4 mb-5">
+            <div class="card h-100">
+              <div class="card-body">
+                <h2 class="card-title">Card One</h2>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                  magni quas ex numquam, maxime minus quam molestias corporis
+                  quod, ea minima accusamus.
+                </p>
+              </div>
+              <div class="card-footer">
+                <a class="btn btn-primary btn-sm" href="#!">
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card h-100">
+              <div class="card-body">
+                <h2 class="card-title">Card Two</h2>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
+                  sunt quam eaque, vel, non in id dolore voluptates quos
+                  eligendi labore.
+                </p>
+              </div>
+              <div class="card-footer">
+                <a class="btn btn-primary btn-sm" href="#!">
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card h-100">
+              <div class="card-body">
+                <h2 class="card-title">Card Three</h2>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                  magni quas ex numquam, maxime minus quam molestias corporis
+                  quod, ea minima accusamus.
+                </p>
+              </div>
+              <div class="card-footer">
+                <a class="btn btn-primary btn-sm" href="#!">
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="py-5 bg-dark">
+        <div class="container px-4 px-lg-5">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Your Website 2021
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }

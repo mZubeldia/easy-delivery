dfd8917918676ebc76b2c412616eb34624aecb21
mZubeldia <mzubeldia09@gmail.com>
2021-08-28 14:01:31 +0200

    add tareas pendientes de completar

--- easy-delivery-bootstrap/src/App.js
+++ easy-delivery-bootstrap/src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 
-import { Button } from "react-bootstrap";
-
 function App() {
   return (
     <div className="App">
@@ -133,7 +131,69 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div class="card text-white bg-secondary my-5 py-4 text-center">
+          <div class="card-body">
+            <p class="text-white m-0">Tareas completadas</p>
+          </div>
+        </div>
+
+        <div class="row gx-4 gx-lg-5">
+          <div class="col-md-4 mb-5">
+            <div class="card h-100">
+              <div class="card-body">
+                <h2 class="card-title">Tarea A</h2>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                  magni quas ex numquam, maxime minus quam molestias corporis
+                  quod, ea minima accusamus.
+                </p>
+              </div>
+              <div class="card-footer">
+                <a class="btn btn-primary btn-sm" href="#!">
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card h-100">
+              <div class="card-body">
+                <h2 class="card-title">Tarea B</h2>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
+                  sunt quam eaque, vel, non in id dolore voluptates quos
+                  eligendi labore.
+                </p>
+              </div>
+              <div class="card-footer">
+                <a class="btn btn-primary btn-sm" href="#!">
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card h-100">
+              <div class="card-body">
+                <h2 class="card-title">Tarea C</h2>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+                  magni quas ex numquam, maxime minus quam molestias corporis
+                  quod, ea minima accusamus.
+                </p>
+              </div>
+              <div class="card-footer">
+                <a class="btn btn-primary btn-sm" href="#!">
+                  More Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <footer class="py-5 bg-dark">
         <div class="container px-4 px-lg-5">
           <p class="m-0 text-center text-white">

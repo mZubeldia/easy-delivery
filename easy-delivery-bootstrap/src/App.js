dfa6821d8a5b70949d8f1b5f7bf9315bd785e593
mZubeldia <mzubeldia09@gmail.com>
2021-08-28 14:02:40 +0200

    change class to className

--- easy-delivery-bootstrap/src/App.js
+++ easy-delivery-bootstrap/src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container px-5">
-            <a class="navbar-brand" href="#!">
+    <div classNameName="App">
+      <header classNameName="App-header">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container px-5">
+            <a className="navbar-brand" href="#!">
               Easy Delivery
             </a>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -18,27 +18,30 @@ function App() {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#!">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#!">
                     Home
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#!">
+                <li className="nav-item">
+                  <a className="nav-link" href="#!">
                     About
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#!">
+                <li className="nav-item">
+                  <a className="nav-link" href="#!">
                     Contact
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#!">
+                <li className="nav-item">
+                  <a className="nav-link" href="#!">
                     Services
                   </a>
                 </li>
@@ -48,83 +51,83 @@ function App() {
         </nav>
       </header>
 
-      <div class="container px-4 px-lg-5">
-        <div class="row gx-4 gx-lg-5 align-items-center my-5">
-          <div class="col-lg-7">
+      <div className="container px-4 px-lg-5">
+        <div className="row gx-4 gx-lg-5 align-items-center my-5">
+          <div className="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
+              className="img-fluid rounded mb-4 mb-lg-0"
               src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
               alt="..."
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Easy Delivery</h1>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Easy Delivery</h1>
             <p>
               This is a template that is great for small businesses. It doesn't
               have too much fancy flare to it, but it makes a great use of the
               standard Bootstrap core components. Feel free to use this template
               for any project you want!
             </p>
-            <a class="btn btn-primary" href="#!">
+            <a className="btn btn-primary" href="#!">
               Call to Action!
             </a>
           </div>
         </div>
 
-        <div class="card text-white bg-secondary my-5 py-4 text-center">
-          <div class="card-body">
-            <p class="text-white m-0">Tareas pendientes de completar</p>
+        <div className="card text-white bg-secondary my-5 py-4 text-center">
+          <div className="card-body">
+            <p className="text-white m-0">Tareas pendientes de completar</p>
           </div>
         </div>
 
-        <div class="row gx-4 gx-lg-5">
-          <div class="col-md-4 mb-5">
-            <div class="card h-100">
-              <div class="card-body">
-                <h2 class="card-title">Tarea 1</h2>
-                <p class="card-text">
+        <div className="row gx-4 gx-lg-5">
+          <div className="col-md-4 mb-5">
+            <div className="card h-100">
+              <div className="card-body">
+                <h2 className="card-title">Tarea 1</h2>
+                <p className="card-text">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
                   magni quas ex numquam, maxime minus quam molestias corporis
                   quod, ea minima accusamus.
                 </p>
               </div>
-              <div class="card-footer">
-                <a class="btn btn-primary btn-sm" href="#!">
+              <div className="card-footer">
+                <a className="btn btn-primary btn-sm" href="#!">
                   More Info
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-md-4 mb-5">
-            <div class="card h-100">
-              <div class="card-body">
-                <h2 class="card-title">Tarea 2</h2>
-                <p class="card-text">
+          <div className="col-md-4 mb-5">
+            <div className="card h-100">
+              <div className="card-body">
+                <h2 className="card-title">Tarea 2</h2>
+                <p className="card-text">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
                   sunt quam eaque, vel, non in id dolore voluptates quos
                   eligendi labore.
                 </p>
               </div>
-              <div class="card-footer">
-                <a class="btn btn-primary btn-sm" href="#!">
+              <div className="card-footer">
+                <a className="btn btn-primary btn-sm" href="#!">
                   More Info
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-md-4 mb-5">
-            <div class="card h-100">
-              <div class="card-body">
-                <h2 class="card-title">Tarea 3</h2>
-                <p class="card-text">
+          <div className="col-md-4 mb-5">
+            <div className="card h-100">
+              <div className="card-body">
+                <h2 className="card-title">Tarea 3</h2>
+                <p className="card-text">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
                   magni quas ex numquam, maxime minus quam molestias corporis
                   quod, ea minima accusamus.
                 </p>
               </div>
-              <div class="card-footer">
-                <a class="btn btn-primary btn-sm" href="#!">
+              <div className="card-footer">
+                <a className="btn btn-primary btn-sm" href="#!">
                   More Info
                 </a>
               </div>
@@ -132,60 +135,60 @@ function App() {
           </div>
         </div>
 
-        <div class="card text-white bg-secondary my-5 py-4 text-center">
-          <div class="card-body">
-            <p class="text-white m-0">Tareas completadas</p>
+        <div className="card text-white bg-secondary my-5 py-4 text-center">
+          <div className="card-body">
+            <p className="text-white m-0">Tareas completadas</p>
           </div>
         </div>
 
-        <div class="row gx-4 gx-lg-5">
-          <div class="col-md-4 mb-5">
-            <div class="card h-100">
-              <div class="card-body">
-                <h2 class="card-title">Tarea A</h2>
-                <p class="card-text">
+        <div className="row gx-4 gx-lg-5">
+          <div className="col-md-4 mb-5">
+            <div className="card h-100">
+              <div className="card-body">
+                <h2 className="card-title">Tarea A</h2>
+                <p className="card-text">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
                   magni quas ex numquam, maxime minus quam molestias corporis
                   quod, ea minima accusamus.
                 </p>
               </div>
-              <div class="card-footer">
-                <a class="btn btn-primary btn-sm" href="#!">
+              <div className="card-footer">
+                <a className="btn btn-primary btn-sm" href="#!">
                   More Info
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-md-4 mb-5">
-            <div class="card h-100">
-              <div class="card-body">
-                <h2 class="card-title">Tarea B</h2>
-                <p class="card-text">
+          <div className="col-md-4 mb-5">
+            <div className="card h-100">
+              <div className="card-body">
+                <h2 className="card-title">Tarea B</h2>
+                <p className="card-text">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
                   tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
                   sunt quam eaque, vel, non in id dolore voluptates quos
                   eligendi labore.
                 </p>
               </div>
-              <div class="card-footer">
-                <a class="btn btn-primary btn-sm" href="#!">
+              <div className="card-footer">
+                <a className="btn btn-primary btn-sm" href="#!">
                   More Info
                 </a>
               </div>
             </div>
           </div>
-          <div class="col-md-4 mb-5">
-            <div class="card h-100">
-              <div class="card-body">
-                <h2 class="card-title">Tarea C</h2>
-                <p class="card-text">
+          <div className="col-md-4 mb-5">
+            <div className="card h-100">
+              <div className="card-body">
+                <h2 className="card-title">Tarea C</h2>
+                <p className="card-text">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
                   magni quas ex numquam, maxime minus quam molestias corporis
                   quod, ea minima accusamus.
                 </p>
               </div>
-              <div class="card-footer">
-                <a class="btn btn-primary btn-sm" href="#!">
+              <div className="card-footer">
+                <a className="btn btn-primary btn-sm" href="#!">
                   More Info
                 </a>
               </div>
@@ -194,9 +197,9 @@ function App() {
         </div>
       </div>
 
-      <footer class="py-5 bg-dark">
-        <div class="container px-4 px-lg-5">
-          <p class="m-0 text-center text-white">
+      <footer className="py-5 bg-dark">
+        <div className="container px-4 px-lg-5">
+          <p className="m-0 text-center text-white">
             Copyright &copy; Easy Delivery 2021
           </p>
         </div>

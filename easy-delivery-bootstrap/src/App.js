72842258b5d119083f3d097975f41dcfbe3b8bfc
mZubeldia <mzubeldia09@gmail.com>
2021-08-29 20:49:09 +0200

    add semantic to landing

--- easy-delivery-bootstrap/src/App.js
+++ easy-delivery-bootstrap/src/App.js
@@ -51,8 +51,8 @@ function App() {
         </nav>
       </header>
 
-      <div className="container px-4 px-lg-5">
-        <div className="row gx-4 gx-lg-5 align-items-center my-5">
+      <main className="container px-4 px-lg-5">
+        <section className="hero-section row gx-4 gx-lg-5 align-items-center my-5">
           <div className="col-lg-7">
             <img
               className="img-fluid rounded mb-4 mb-lg-0"
@@ -72,130 +72,133 @@ function App() {
               Call to Action!
             </a>
           </div>
-        </div>
+        </section>
 
-        <div className="card text-white bg-secondary my-5 py-4 text-center">
-          <div className="card-body">
-            <p className="text-white m-0">Tareas pendientes de completar</p>
+        <section className="pending-section">
+          <div className="card text-white bg-secondary my-5 py-4 text-center">
+            <div className="card-body">
+              <p className="text-white m-0">Tareas pendientes de completar</p>
+            </div>
           </div>
-        </div>
 
-        <div className="row gx-4 gx-lg-5">
-          <div className="col-md-4 mb-5">
-            <div className="card h-100">
-              <div className="card-body">
-                <h2 className="card-title">Tarea 1</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
+          <div className="row gx-4 gx-lg-5">
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title">Tarea 1</h2>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem magni quas ex numquam, maxime minus quam molestias
+                    corporis quod, ea minima accusamus.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a className="btn btn-primary btn-sm" href="#!">
+                    More Info
+                  </a>
+                </div>
               </div>
-              <div className="card-footer">
-                <a className="btn btn-primary btn-sm" href="#!">
-                  More Info
-                </a>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title">Tarea 2</h2>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quod tenetur ex natus at dolorem enim! Nesciunt pariatur
+                    voluptatem sunt quam eaque, vel, non in id dolore voluptates
+                    quos eligendi labore.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a className="btn btn-primary btn-sm" href="#!">
+                    More Info
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title">Tarea 3</h2>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem magni quas ex numquam, maxime minus quam molestias
+                    corporis quod, ea minima accusamus.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a className="btn btn-primary btn-sm" href="#!">
+                    More Info
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-md-4 mb-5">
-            <div className="card h-100">
-              <div className="card-body">
-                <h2 className="card-title">Tarea 2</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
-                  sunt quam eaque, vel, non in id dolore voluptates quos
-                  eligendi labore.
-                </p>
-              </div>
-              <div className="card-footer">
-                <a className="btn btn-primary btn-sm" href="#!">
-                  More Info
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4 mb-5">
-            <div className="card h-100">
-              <div className="card-body">
-                <h2 className="card-title">Tarea 3</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
-              </div>
-              <div className="card-footer">
-                <a className="btn btn-primary btn-sm" href="#!">
-                  More Info
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        </section>
 
-        <div className="card text-white bg-secondary my-5 py-4 text-center">
-          <div className="card-body">
-            <p className="text-white m-0">Tareas completadas</p>
+        <section className="done-section">
+          <div className="card text-white bg-secondary my-5 py-4 text-center">
+            <div className="card-body">
+              <p className="text-white m-0">Tareas completadas</p>
+            </div>
           </div>
-        </div>
-
-        <div className="row gx-4 gx-lg-5">
-          <div className="col-md-4 mb-5">
-            <div className="card h-100">
-              <div className="card-body">
-                <h2 className="card-title">Tarea A</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
+          <div className="row gx-4 gx-lg-5">
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title">Tarea A</h2>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem magni quas ex numquam, maxime minus quam molestias
+                    corporis quod, ea minima accusamus.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a className="btn btn-primary btn-sm" href="#!">
+                    More Info
+                  </a>
+                </div>
               </div>
-              <div className="card-footer">
-                <a className="btn btn-primary btn-sm" href="#!">
-                  More Info
-                </a>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title">Tarea B</h2>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quod tenetur ex natus at dolorem enim! Nesciunt pariatur
+                    voluptatem sunt quam eaque, vel, non in id dolore voluptates
+                    quos eligendi labore.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a className="btn btn-primary btn-sm" href="#!">
+                    More Info
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-5">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h2 className="card-title">Tarea C</h2>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem magni quas ex numquam, maxime minus quam molestias
+                    corporis quod, ea minima accusamus.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <a className="btn btn-primary btn-sm" href="#!">
+                    More Info
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-md-4 mb-5">
-            <div className="card h-100">
-              <div className="card-body">
-                <h2 className="card-title">Tarea B</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
-                  sunt quam eaque, vel, non in id dolore voluptates quos
-                  eligendi labore.
-                </p>
-              </div>
-              <div className="card-footer">
-                <a className="btn btn-primary btn-sm" href="#!">
-                  More Info
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4 mb-5">
-            <div className="card h-100">
-              <div className="card-body">
-                <h2 className="card-title">Tarea C</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
-              </div>
-              <div className="card-footer">
-                <a className="btn btn-primary btn-sm" href="#!">
-                  More Info
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        </section>
+      </main>
 
       <footer className="py-5 bg-dark">
         <div className="container px-4 px-lg-5">

0d7374db6fb0d9f0e97fde8b3ffab4fa351cb4d7
mZubeldia <mzubeldia09@gmail.com>
2021-09-06 11:08:54 +0200

    move sides of title and burger menu to opposite sides on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,8 @@ const Header = () => {
     <header classNameName="App-header">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container px-5">
-          <a className="navbar-brand" href="/">
-            Easy Delivery
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   Home
@@ -54,6 +39,21 @@ const Header = () => {
               </li>
             </ul>
           </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <a className="navbar-brand" href="/">
+            Easy Delivery
+          </a>
         </div>
       </nav>
     </header>

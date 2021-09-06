fdeab88fe029ffb73c6f30c4f1e32eb1c80c4c92
mZubeldia <mzubeldia09@gmail.com>
2021-09-06 20:30:55 +0200

    edit content tasks in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,11 +20,14 @@ const Home = () => {
             <div className="card h-100 border-warning">
               <div className="card-body">
                 <h2 className="card-title">Tarea 1 +</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item  mb-2">Fecha de recogida</li>
+                  <li className="list-group-item mb-2">Fecha de entrega</li>
+                  <li className="list-group-item mb-2">
+                    Direccion de recogida
+                  </li>
+                  <li className="list-group-item mb-2">Direccion de entrega</li>
+                </ul>
               </div>
               <div className="card-footer">
                 <Link className="btn btn-warning btn-sm" to="/detalle-tarea">
@@ -38,12 +41,14 @@ const Home = () => {
             <div className="card h-100 border-warning">
               <div className="card-body">
                 <h2 className="card-title">Tarea 2</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
-                  sunt quam eaque, vel, non in id dolore voluptates quos
-                  eligendi labore.
-                </p>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item  mb-2">Fecha de recogida</li>
+                  <li className="list-group-item mb-2">Fecha de entrega</li>
+                  <li className="list-group-item mb-2">
+                    Direccion de recogida
+                  </li>
+                  <li className="list-group-item mb-2">Direccion de entrega</li>
+                </ul>
               </div>
               <div className="card-footer">
                 <a className="btn btn-warning btn-sm" href="#!">
@@ -56,11 +61,14 @@ const Home = () => {
             <div className="card h-100 border-warning">
               <div className="card-body">
                 <h2 className="card-title">Tarea 3</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item  mb-2">Fecha de recogida</li>
+                  <li className="list-group-item mb-2">Fecha de entrega</li>
+                  <li className="list-group-item mb-2">
+                    Direccion de recogida
+                  </li>
+                  <li className="list-group-item mb-2">Direccion de entrega</li>
+                </ul>
               </div>
               <div className="card-footer">
                 <a className="btn btn-warning btn-sm" href="#!">
@@ -83,11 +91,14 @@ const Home = () => {
             <div className="card h-100 border-success">
               <div className="card-body">
                 <h2 className="card-title">Tarea A</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item  mb-2">Fecha de recogida</li>
+                  <li className="list-group-item mb-2">Fecha de entrega</li>
+                  <li className="list-group-item mb-2">
+                    Direccion de recogida
+                  </li>
+                  <li className="list-group-item mb-2">Direccion de entrega</li>
+                </ul>
               </div>
               <div className="card-footer">
                 <a className="btn btn-success btn-sm" href="#!">
@@ -100,12 +111,14 @@ const Home = () => {
             <div className="card h-100 border-success">
               <div className="card-body">
                 <h2 className="card-title">Tarea B</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem
-                  sunt quam eaque, vel, non in id dolore voluptates quos
-                  eligendi labore.
-                </p>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item  mb-2">Fecha de recogida</li>
+                  <li className="list-group-item mb-2">Fecha de entrega</li>
+                  <li className="list-group-item mb-2">
+                    Direccion de recogida
+                  </li>
+                  <li className="list-group-item mb-2">Direccion de entrega</li>
+                </ul>
               </div>
               <div className="card-footer">
                 <a className="btn btn-success btn-sm" href="#!">
@@ -118,11 +131,14 @@ const Home = () => {
             <div className="card h-100 border-success">
               <div className="card-body">
                 <h2 className="card-title">Tarea C</h2>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
-                  magni quas ex numquam, maxime minus quam molestias corporis
-                  quod, ea minima accusamus.
-                </p>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item  mb-2">Fecha de recogida</li>
+                  <li className="list-group-item mb-2">Fecha de entrega</li>
+                  <li className="list-group-item mb-2">
+                    Direccion de recogida
+                  </li>
+                  <li className="list-group-item mb-2">Direccion de entrega</li>
+                </ul>
               </div>
               <div className="card-footer">
                 <a className="btn btn-success btn-sm" href="#!">

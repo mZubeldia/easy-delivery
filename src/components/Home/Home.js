3271a907f1f3426bc68484e2ab7babc4b15cb025
mZubeldia <mzubeldia09@gmail.com>
2021-09-21 17:10:03 +0200

    change colors for completed section home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,14 +9,14 @@ const Home = (props) => {
       {/* done section */}
 
       <section className="done-section">
-        <div className="card text-white bg-success my-5 py-4 text-center">
+        <div className="card text-white bg-completed my-5 py-4 text-center">
           <div className="card-body">
             <h3 className="text-white m-0">Tareas completadas</h3>
           </div>
         </div>
         <div className="row gx-4 gx-lg-5">
           <div className="col-md-4 mb-5">
-            <div className="card h-100 border-success">
+            <div className="card h-100 border-completed">
               <div className="card-body">
                 <h2 className="card-title">Tarea A</h2>
                 <ul className="list-group list-group-flush">
@@ -28,13 +28,9 @@ const Home = (props) => {
                   <li className="list-group-item mb-2">Direccion de entrega</li>
                 </ul>
               </div>
-              <div className="card-footer">
-                <a className="btn btn-success btn-sm" href="#!">
-                  More Info
-                </a>
-              </div>
             </div>
           </div>
+
           <div className="col-md-4 mb-5">
             <div className="card h-100 border-success">
               <div className="card-body">

5a26751d067b0df0a389860b83a075ada3018536
mZubeldia <mzubeldia09@gmail.com>
2021-09-21 20:22:58 +0200

    change footer bg color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-5 bg-dark">
+    <footer className="py-5 bg-black2">
       <div className="container px-4 px-lg-5">
         <p className="m-0 text-center text-white">
           Copyright &copy; Easy Delivery 2021

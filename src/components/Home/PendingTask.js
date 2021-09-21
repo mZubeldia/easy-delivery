fbf63b31cfc221181f8da790ea257ce86381298e
mZubeldia <mzubeldia09@gmail.com>
2021-09-21 16:57:30 +0200

    add reset to a elements css

--- src/components/Home/PendingTask.js
+++ src/components/Home/PendingTask.js
@@ -9,30 +9,38 @@ const PendingTask = (props) => {
   date = new Date('2013-03-10T02:00:00Z');*/
   return (
     <>
-      <div key={props.pendingTask.id} className="card h-100 border-warning">
-        <div className="card-body">
-          <h2 className="card-title">Tarea {props.pendingTask.id}</h2>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item  mb-2">
-              Fecha recogida: {props.pendingTask.pickUpDate}
-            </li>
-            <li className="list-group-item mb-2">{props.pendingTask.farm}</li>
-            <li className="list-group-item mb-2">
-              Fecha entrega: {props.pendingTask.dropOffDate}
-            </li>
-            <li className="list-group-item mb-2">
-              {props.pendingTask.abattoir}
-            </li>
-          </ul>
-        </div>
-        <div className="card-footer">
-          <Link className="btn btn-warning btn-sm" to="/detalle-tarea">
-            More Info
-          </Link>
-        </div>
+      <div key={props.pendingTask.id} className="card h-100 border-pending">
+        <Link className="card-link" to="/detalle-tarea">
+          <div className="card-body">
+            <h2 className="card-title">Tarea {props.pendingTask.id}</h2>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item  mb-2">
+                Fecha recogida: {props.pendingTask.pickUpDate}
+              </li>
+              <li className="list-group-item mb-2">{props.pendingTask.farm}</li>
+              <li className="list-group-item mb-2">
+                Fecha entrega: {props.pendingTask.dropOffDate}
+              </li>
+              <li className="list-group-item mb-2">
+                {props.pendingTask.abattoir}
+              </li>
+            </ul>
+          </div>
+
+          <div className="card-footer"></div>
+        </Link>
       </div>
     </>
   );
 };
 
 export default PendingTask;
+
+/**
+ *  <div className="card-footer"></div>
+        <Link className="btn btn-warning btn-sm" to="/detalle-tarea">
+          More Info
+        </Link>
+      </div>
+ * 
+ */
